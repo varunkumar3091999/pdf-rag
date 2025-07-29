3ea51edf5661bf050122198608de5aa7cc83511f
varun <varunkumar0930@gmail.com>
2025-07-29 22:28:23 +0530

    sqlite to chroma

--- read_excel.py
+++ read_excel.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import sqlite3
+import sqlite3
+from langchain.schema import Document
+from langchain_chroma import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
 
 
 def read_csv(filepath):
@@ -25,7 +29,44 @@ def read_excel(filepath, sheet_name=0):
   
 def add_to_db(df, filepath):
   conn = sqlite3.connect("excel_data.db")
-  df.to_sql('mytable', conn, if_exists="replace",index=False)
+  df.to_sql('car_price_dataset', conn, if_exists="replace",index=False)
+  
+
+def fetch_rows_from_sqlite(db_path, table_name):
+   conn = sqlite3.connect(db_path)
+   cursor = conn.cursor()
+   cursor.execute(f"select * from {table_name}")
+   columns = [desc[0] for desc in cursor.description] 
+   rows = cursor.fetchall()
+   conn.close()
+   
+   documents = []
+   for row in rows:
+      text = "\n".join([f"{col}:{val}" for col, val in zip(columns, row)])
+      documents.append(text)      
+   return documents
+
+
+def convert_to_document(text_chunks):
+   return [Document(page_content=chunk) for chunk in text_chunks]
+
+def get_embedding_function():
+    embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    return embeddings
+ 
+db = Chroma(
+  persist_directory="database", embedding_function=get_embedding_function()
+)
+
+def embed_and_store(documents):
+   embeddings = get_embedding_function()
+   vector_db = Chroma.from_documents(documents, embeddings, persist_directory="chroma_db_excel")
+   print("Data stored to Chroma")
+   return vector_db
 
 read_excel("./excel/car_price_dataset.xlsx")
-# read_csv("./excel/car_price_dataset.csv")
+text = fetch_rows_from_sqlite("excel_data.db","car_price_dataset")
+print(text, "text")
+docs = convert_to_document(text)
+print(docs, "docs")
+embed_and_store(docs)
