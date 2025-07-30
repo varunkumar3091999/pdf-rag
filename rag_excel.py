1e99e53de943ee7ff08685adb3422c32a835d29c
varun <varunkumar0930@gmail.com>
2025-07-30 22:04:10 +0530

    nlp to sql query

--- rag_excel.py
+++ rag_excel.py
@@ -1,14 +1,29 @@
-from langchain_core.prompts import ChatPromptTemplate
 from langchain_ollama import OllamaLLM
+from langchain.utilities import SQLDatabase
+from langchain_experimental.sql import SQLDatabaseChain
 
-model = OllamaLLM(model='mistral')
+db_path = "sqlite:///excel_data.db"
+print(1)
 
-def llm_generate_sql(user_question, schema_description):
-   prompt = f"""You are an expert that writes correct SQL SELECT queries.
-    Schema:
-    {schema_description}
-    Convert this question to an SQL SELECT statement:
-    Question: {user_question}
-    SQL:"""
-    
-    # response_text = model.invoke(prompt)
+db = SQLDatabase.from_uri(db_path)
+print(2)
+
+llm = OllamaLLM(model='mistral')
+print(3)
+
+sql_chain = SQLDatabaseChain.from_llm(llm, db, verbose=True)
+print(4)
+
+question = "How many cars have fuel type petrol?"
+print(5)
+
+prompt = "Convert the natural language question: 'How many cars have fuel type petrol?' into a SQL query on a table named car_price_dataset with a 'fuel_type' column."
+print(6)
+
+# answer = sql_chain.invoke(prompt)
+
+response = llm.invoke(prompt)
+print(response)
+
+print("Answer:", response)
+
