813aa60625820ac1e676b9fa8fc7a4bf3c110779
varun <varunkumar0930@gmail.com>
2025-08-08 22:08:49 +0530

    excel to sentence to document chunks

--- rag_excel.py
+++ rag_excel.py
@@ -1,29 +1,98 @@
-from langchain_ollama import OllamaLLM
-from langchain.utilities import SQLDatabase
-from langchain_experimental.sql import SQLDatabaseChain
+import pandas as pd
+import ollama
+import chromadb
+from langchain.schema.document import Document
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
 
-db_path = "sqlite:///excel_data.db"
-print(1)
 
-db = SQLDatabase.from_uri(db_path)
-print(2)
 
-llm = OllamaLLM(model='mistral')
-print(3)
 
-sql_chain = SQLDatabaseChain.from_llm(llm, db, verbose=True)
-print(4)
+excel_file_path = 'excel/car_price_dataset.xlsx'
 
-question = "How many cars have fuel type petrol?"
-print(5)
+df = pd.read_excel(excel_file_path)
 
-prompt = "Convert the natural language question: 'How many cars have fuel type petrol?' into a SQL query on a table named car_price_dataset with a 'fuel_type' column."
-print(6)
 
-# answer = sql_chain.invoke(prompt)
+print(df.iterrows())
 
-response = llm.invoke(prompt)
-print(response)
+def row_to_sentence():
+    sentences = []
 
-print("Answer:", response)
+    for idx, row in df.iterrows():
+        print(idx, "index")
+        sentence = (
+        f"{row['Year']} {row['Brand']} {row['Model']} with a {row['Engine_Size']}L "
+        f"{row['Fuel_Type']} engine, {row['Transmission']} transmission, "
+        f"{row['Doors']} doors, {row['Owner_Count']} previous owners, "
+        f"total distance driven {row['Total Distance']} km, "
+        f"priced at {row['Price']}."
+        )
+        sentences.append(sentence)
+        
+    return sentences
+
+
+def split_documents(documents: list[Document]):
+  text_splitter = CharacterTextSplitter(
+    chunk_size=100, 
+    chunk_overlap=0, 
+    # saperators=['\n']
+  )
+  
+  return text_splitter.split_documents(documents)
+
+
+def get_embedding_function():
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    return embeddings
+  
+
+db = Chroma(
+  persist_directory="database", embedding_function=get_embedding_function()
+)
+
+
+
+
+# def add_to_chroma(chunks: list[Document]):
+#   last_page_id = None
+#   current_chunk_index = 0
+  
+#   new_chunks = []
+#   new_chunk_ids = []
+  
+#   for chunk  in chunks:
+#     source = chunk.metadata.get("source")
+#     page = chunk.metadata.get("page")
+#     current_page_id = f"{source}:{page}"
+#     if(current_page_id == last_page_id):
+#       current_chunk_index += 1
+#     else:
+#       current_chunk_index = 0
+#     chunk_id = f"{current_page_id}:{current_chunk_index}"
+#     last_page_id = current_page_id
+#     new_chunk_ids.append(chunk_id)
+#     chunk.metadata["id"] = chunk_id
+#     new_chunks.append(chunk)
+
+#   existing_items = db.get(include=[])
+  
+#   print(f"Number of documents in DB: {len(new_chunk_ids)}")
+#   # for chunk in chunk_id
+
+#   db.add_documents(new_chunks,ids=new_chunk_ids)
+#   return f"Number of documents in DB: {len(new_chunk_ids)}"
+#   # db.persist()
+  
+  
+  
+sentenses = row_to_sentence()
+docs = [Document(page_content=sentence, ) for ind,sentence in sentenses]
+
+chunks = split_documents(docs)
+
+
+print(chunks, "chunksc")
+# add_to_chroma(chunks)
 
